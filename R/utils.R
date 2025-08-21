51a84aecdeed0a75aa10686fff958b8541b74669
dgkf <18220321+dgkf@users.noreply.github.com>
2025-08-21 13:56:29 -0400

    chore(lint): fix indentation

--- R/utils.R
+++ R/utils.R
@@ -1,40 +1,40 @@
 `%||%` <- function(lhs, rhs) {
-    if (is.null(lhs)) rhs else lhs
+  if (is.null(lhs)) rhs else lhs
 }
 
 vlapply <- function(
-    ...,
-    FUN.VALUE = logical(1L) # nolint: object_name_linter.
+  ...,
+  FUN.VALUE = logical(1L) # nolint: object_name_linter.
 ) {
-    vapply(..., FUN.VALUE = FUN.VALUE)
+  vapply(..., FUN.VALUE = FUN.VALUE)
 }
 
 vcapply <- function(
-    ...,
-    FUN.VALUE = character(1L) # nolint: object_name_linter.
+  ...,
+  FUN.VALUE = character(1L) # nolint: object_name_linter.
 ) {
-    vapply(..., FUN.VALUE = FUN.VALUE)
+  vapply(..., FUN.VALUE = FUN.VALUE)
 }
 
 viapply <- function(
-    ...,
-    FUN.VALUE = integer(1L) # nolint: object_name_linter.
+  ...,
+  FUN.VALUE = integer(1L) # nolint: object_name_linter.
 ) {
-    vapply(..., FUN.VALUE = FUN.VALUE)
+  vapply(..., FUN.VALUE = FUN.VALUE)
 }
 
 
 vnapply <- function(
-    ...,
-    FUN.VALUE = numeric(1L) # nolint: object_name_linter.
+  ...,
+  FUN.VALUE = numeric(1L) # nolint: object_name_linter.
 ) {
-    vapply(..., FUN.VALUE = FUN.VALUE)
+  vapply(..., FUN.VALUE = FUN.VALUE)
 }
 
 tocamel <- function(x, ...) {
-    paste0(toupper(substring(x, 1, 1)), tolower(substring(x, 2)))
+  paste0(toupper(substring(x, 1, 1)), tolower(substring(x, 2)))
 }
 
 rversion <- function() {
-    numeric_version(paste(collapse = ".", R.version[c("major", "minor")]))
+  numeric_version(paste(collapse = ".", R.version[c("major", "minor")]))
 }
