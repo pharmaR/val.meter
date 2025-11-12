6294c8a3eb9d8b9b9f90249cbb17ef7662ab1400
Jeff Thompson <160783290+jthompson-arcus@users.noreply.github.com>
2025-11-12 17:27:59 -0500

    Add `web_html` data for `repo_resource` class (#23)
    * feat: `web_html` data for `repo_resource`
    
    * Fix gripes from lint GHA
    
    * Remove a couple more trailing whitespaces
    
    * Implement only from `cran_repo_resource`
    
    * Resolve lintr error
    
    * Don't use contrib source for `pkg@repo`
    
    * Minor adjustments for cleaner code
    
    * Check MD5sum of `repo_resource` for `cran_repo_resource` conversion
    
    * Fix `lintr` issue

---
+++ R/data_web_html.R
@@ -0,0 +1,27 @@
+#' @include impl_data.R
+
+impl_data(
+  "web_url",
+  class = class_character,
+  for_resource = cran_repo_resource,
+  function(pkg, resource, ...) {
+    sprintf(
+      "%s/web/packages/%s",
+      resource@repo,
+      resource@package
+    )
+  }
+)
+
+impl_data(
+  "web_html",
+  class = c("xml_document", "xml_node"),
+  for_resource = cran_repo_resource,
+  permissions = "network",
+  function(pkg, resource, ...) {
+    pkg$web_url |>
+      httr2::request() |>
+      httr2::req_perform() |>
+      httr2::resp_body_html()
+  }
+)
---
+++ R/utils_mirrors.R
@@ -0,0 +1,30 @@
+#' Retrieves local CRAN mirrors
+#'
+#' Retrieves local CRAN mirrors including RStudio CRAN mirror.
+#'
+#' @inheritParams utils::getCRANmirrors
+#'
+#' @keywords internal
+#' @noRd
+get_cran_mirrors <- function(all = FALSE, local.only = TRUE) { # nolint
+  cran_mirrors <- getCRANmirrors(all = all, local.only = local.only)
+  # NOTE: For the time being the POSIT CRAN mirror is being manually
+  # added. It's inclusion is justified since it is a true cran mirror and
+  # is the default in very populat IDEs, namely RStudio and Positron.
+  cran_mirrors <- rbind(
+    cran_mirrors,
+    data.frame(
+      Name = "RStudio IDE",
+      Country = character(1),
+      City = character(1),
+      URL = "https://cran.rstudio.com/",
+      Host = character(1),
+      Maintainer = character(1),
+      OK = integer(1),
+      CountryCode = character(1),
+      Comment = character(1)
+    )
+  )
+
+  cran_mirrors
+}
