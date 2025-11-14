5f49059144265f414618d8a32eaec0adb5df85ea
Doug Kelkhoff <18220321+dgkf@users.noreply.github.com>
2025-11-14 12:44:13 -0500

    feat: add coverage metrics (#28)

---
+++ R/data_coverage.R
@@ -0,0 +1,60 @@
+impl_data(
+  "covr_coverage",
+  class = S7::new_S3_class("coverage"),
+  tags = c("execution"),
+  suggests = "covr",
+  permissions = "execution"
+)
+
+impl_data(
+  "covr_coverage",
+  for_resource = local_source_resource,
+  function(pkg, resource, field, ..., quiet = opt("quiet")) {
+    covr::package_coverage(resource@path, type = "tests", quiet = quiet)
+  }
+)
+
+impl_data(
+  "test_expression_coverage_fraction",
+  metric = TRUE,
+  class = class_double,
+  title = "Test coverage of package code by expression",
+  description = paste0(
+    "The fraction of expressions of package code that are evaluated by any ",
+    "test"
+  ),
+  function(pkg, resource, field, ..., quiet = opt("quiet")) {
+    tally <- covr::tally_coverage(pkg$covr_coverage, by = "expression")
+    mean(tally$value > 0)
+  }
+)
+
+impl_data(
+  "test_expression_coverage_fraction",
+  for_resource = mock_resource,
+  function(pkg, resource, field, ...) {
+    1 - pmin(pmax(rnorm(1, 0.5, 0.45)**2, 0), 1)
+  }
+)
+
+impl_data(
+  "test_line_coverage_fraction",
+  metric = TRUE,
+  class = class_double,
+  title = "Test coverage of package code by line",
+  description = paste0(
+    "The fraction of lines of package code that are evaluated by any test"
+  ),
+  function(pkg, resource, field, ..., quiet = opt("quiet")) {
+    tally <- covr::tally_coverage(pkg$covr_coverage, by = "line")
+    mean(tally$value > 0)
+  }
+)
+
+impl_data(
+  "test_line_coverage_fraction",
+  for_resource = mock_resource,
+  function(pkg, resource, field, ...) {
+    1 - pmin(pmax(rnorm(1, 0.5, 0.45)**2, 0), 1)
+  }
+)
