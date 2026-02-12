bc14bbb5209d6317e3e13ceff13f8585b254e77e
dgkf <18220321+dgkf@users.noreply.github.com>
2026-02-12 16:03:48 -0500

    feat: improve output for knitr knit_print

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(lib, pkg) {
   S7::methods_register()
+  s3_register("knitr::knit_print", "evaluate_evaluation")
 }
 
 # Unclear why this is needed, but without it we get an R CMD check NOTE
