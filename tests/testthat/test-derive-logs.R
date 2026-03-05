ede9e84b373c451cc235ddf81bf3c2220c319906
dgkf <18220321+dgkf@users.noreply.github.com>
2026-03-05 08:55:42 -0500

    chore: add tests for order independence of logs

--- tests/testthat/test-derive-logs.R
+++ tests/testthat/test-derive-logs.R
@@ -110,3 +110,81 @@ test_that("logging can be disabled by global option", {
 
   expect_true(is.null(logs <- p@logs[["logs_test_name_character_count"]]))
 })
+
+test_that("logging captures logs for the deepest evaluated metric", {
+  # in situations where one metric requires the evaluation of another metric,
+  # the output should be attributed to the lowest evaluated metric on the call
+  # stack
+
+  old <- options(val.meter.logs = TRUE)
+  on.exit(options(old))
+
+  impl_data(
+    "logs_random_word",
+    metric = TRUE,
+    class = class_character,
+    overwrite = TRUE,
+    quiet = TRUE,
+    function(pkg, resource, field, ...) {
+      cat("inner_cat\n")
+      message("inner_message")
+      chars <- sample(letters, round(runif(1, 20, 40)), replace = TRUE)
+      paste(chars, collapse = "")
+    }
+  )
+
+  impl_data(
+    "logs_random_word_character_count",
+    metric = TRUE,
+    class = class_integer,
+    overwrite = TRUE,
+    quiet = TRUE,
+    function(pkg, resource, field, ...) {
+      cat("outer_cat1\n")
+      message("outer_message1")
+      warning("outer_warning1")
+      cli::cat_line(cli::col_blue("blue"))
+      out <- nchar(pkg$logs_random_word)
+      cat("outer_cat2\n")
+      message("outer_message2") # after returning to outer evaluation
+      out
+    }
+  )
+
+  # expect that logs were captured
+  p1 <- pkg(mock_resource(package = "test", version = "1.2.3"))
+  p2 <- pkg(mock_resource(package = "test", version = "1.2.3"))
+
+  # first evaluate outer metric, which will call inner metric
+  expect_silent(p1$logs_random_word_character_count)
+  expect_silent({
+    p2$logs_random_word
+    p2$logs_random_word_character_count
+  })
+
+  # expect logs to be execution order independent
+  expect_identical(
+    p1@logs$logs_random_word_character_count,
+    p2@logs$logs_random_word_character_count
+  )
+  expect_identical(
+    p1@logs$logs_random_word,
+    p2@logs$logs_random_word
+  )
+
+  # expect that inner logs capture messages from inner metric
+  expect_length(p1@logs$logs_random_word, 2L)
+  expect_match(p1@logs$logs_random_word[[1L]], "inner_cat")
+  expect_match(
+    conditionMessage(p1@logs$logs_random_word[[2L]]),
+    "inner_message"
+  )
+
+  # expect that outer logs capture its output after returning from inner capture
+  expect_length(p1@logs$logs_random_word_character_count, 5L)
+  expect_match(p1@logs$logs_random_word_character_count[[4L]], "outer_cat2")
+  expect_match(
+    conditionMessage(p1@logs$logs_random_word_character_count[[5L]]),
+    "outer_message2"
+  )
+})
